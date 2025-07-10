298aae7e7dcd346df0d5f5faf84819bab43144f8
macebook <macebook@macebooks-MacBook-Pro.local>
2025-07-10 22:30:48 +0700

    update about

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,19 +1,16 @@
-
-
-
 export default function AboutSection() {
   return (
-    <section className="bg-white py-16 px-4">
-      <div className="max-w-4xl mx-auto text-center">
-      <h2 className="text-3xl sm:text-6xl font-black leading-tight text-[#1F3329] tracking-tight mb-6">
-            ✨ Về<br /> ĐiĐi Camping
-          </h2>
-          <p className="text-gray-700 text-lg sm:text-xl leading-relaxed">
-            Một hành trình trở về với thiên nhiên – nơi chỉ cần một chiếc lều, một ngọn đồi, và tâm hồn tự do.
-             ĐiĐi mang đến dịch vụ trọn gói, để bạn chỉ việc tận hưởng từng khoảnh khắc hoang sơ.
-          </p>
+    <section className="bg-white py-12 px-4">
+      <div className="max-w-4xl mx-auto text-center font-sans">
+        <h2 className="text-3xl sm:text-6xl font-black leading-tight text-[#1F3329] tracking-tight mb-6">
+          ✨ Về<br /> ĐiĐi Camping
+        </h2>
+        <p className="text-gray-700 text-lg sm:text-xl leading-relaxed tracking-wide">
+          Về với thiên nhiên giúp ta khỏe mạnh và hạnh phúc hơn.  
+          <br />
+          Đó là lý do ĐiĐi Camping ra đời – để bạn dễ dàng thuê lều trại và phụ kiện cắm trại chất lượng, sạch sẽ, và an tâm lên đường hòa mình vào thiên nhiên hoang sơ, nơi có thể tạm ngắt kết nối và thật sự tận hưởng.
+        </p>
       </div>
     </section>
   )
 }
-
